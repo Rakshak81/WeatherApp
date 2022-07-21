6c1ee5177f76d8ec5298413a05c1f7dbe2f624e0
Rakshak <rakshakshetty@Rakshaks-MacBook-Pro.local>
2022-07-21 18:17:56 +0100

    display

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let weather = {
-    "apikey": "6c06887f3d3fc09b70eb5b9797c77203",
-    fetchWeather: function () {
+    apikey: "6c06887f3d3fc09b70eb5b9797c77203",
+    fetchWeather: function (city) {
         fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=6c06887f3d3fc09b70eb5b9797c77203"
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apikey
         )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
     },
+    displayWeather: function(data) {
+       const name = data.name;
+       const description = data.weather[0].description;
+       const temp = data.main.temp;
+       console.log(name,description, temp)
+       document.querySelector(".city").innerText = name ;
+       document.querySelector(".description").innerText = description;
+       document.querySelector(".temperature").innerText = temp + " °C ";
+    }
+
 };
